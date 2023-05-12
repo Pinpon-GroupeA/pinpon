dba8b6947fe74d679d0b09f15f40ed0565392965
Xorko <thib.legoff@gmail.com>
2023-05-12 15:57:57 +0200

    feat(drone): add fallback to components

--- app/intervention/[id]/(tabs)/drone/images.tsx
+++ app/intervention/[id]/(tabs)/drone/images.tsx
@@ -1,4 +1,5 @@
 import { useLocalSearchParams } from 'expo-router';
+import { Heading } from 'native-base';
 import Gallery from '../../../../../components/images/gallery';
 
 export default function Images() {
@@ -7,4 +8,6 @@ export default function Images() {
   if (lon && lat) {
     return <Gallery latitude={isNaN(Number(lat)) ? Number(lat[0]) : Number(lat)} longitude={isNaN(Number(lon)) ? Number(lon[0]) : Number(lon)} />;
   }
+
+  return <Heading>Erreur: Pas de latitude ou longitude</Heading>
 }
