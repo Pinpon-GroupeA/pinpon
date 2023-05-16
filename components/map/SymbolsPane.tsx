2adec311e8aeae3d4989c3542796b2b6198820c1
Agogue <agoguebenoit@gmail.com>
2023-05-16 13:39:23 +0200

    feat: add means request button on map page

--- components/map/SymbolsPane.tsx
+++ components/map/SymbolsPane.tsx
@@ -1,21 +1,29 @@
-import { Box, Divider, ScrollView } from 'native-base';
+import { useQuery } from '@tanstack/react-query';
+import { Box, Button, Divider, Modal, ScrollView } from 'native-base';
 import { useState } from 'react';
 
 import ColorSelection from './ColorSelection';
 import FireFighterSymbolList from './FireFighterSymbolList';
 import SymbolList from './SymbolList';
-import { InterventionMean } from '../../types/mean-types';
+import { InterventionMean, MeanType } from '../../types/mean-types';
+import { fetchMeansTypes } from '../../utils/means-type';
+import ModalMeansRequest from '../means/mean-request/ModalMeansRequest';
 
 type SymbolsPaneProps = {
   fireFighterMeans: InterventionMean[];
 };
 
 const SymbolsPane = ({ fireFighterMeans }: SymbolsPaneProps) => {
-  const [openModal, setOpenModal] = useState<boolean>(false);
+  const [openColorSelection, setOpenColorSelection] = useState<boolean>(false);
+  const [openSymbolSelection, setOpenSymbolSelection] = useState<boolean>(false);
+
+  const { data: meanTypes } = useQuery<MeanType[]>(['meanTypes'], {
+    queryFn: fetchMeansTypes,
+  });
 
   return (
     <ScrollView>
-      <ColorSelection isOpen={openModal} onClose={() => setOpenModal(false)} />
+      <ColorSelection isOpen={openColorSelection} onClose={() => setOpenColorSelection(false)} />
       <Box flexDir="row">
         <Box width="50%">
           <FireFighterSymbolList fireFighterMeans={fireFighterMeans} />
@@ -25,6 +33,14 @@ const SymbolsPane = ({ fireFighterMeans }: SymbolsPaneProps) => {
           <SymbolList />
         </Box>
       </Box>
+      <Button onPress={() => () => setOpenSymbolSelection(true)}>Ajouter un symbole</Button>
+      <Modal isOpen={openSymbolSelection} onClose={() => setOpenSymbolSelection(false)} size="md">
+        <ModalMeansRequest
+          meanTypes={meanTypes || []}
+          setShowModal={setOpenSymbolSelection}
+          key={meanTypes?.length}
+        />
+      </Modal>
     </ScrollView>
   );
 };
